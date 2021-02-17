022947eeda73259afd41e3adc7ee46a56dc8c8be
hogen <dannyliesco@gmail.com>
2021-02-18 06:58:29 +0800

    update to chapter 3.6 using lateinit to init lately.

---
+++ com/kotlincookbook/hogen/3oopinkotlin.kt
@@ -0,0 +1,28 @@
+package com.kotlincookbook.hogen
+
+import org.apache.commons.math3.complex.Complex
+
+//overload operator
+data class Point(val x: Int,val y: Int)
+
+operator fun Point.unaryMinus() = Point(-x,-y)
+
+infix fun Point.newfun(x:Int) = println("a new func${x}")
+
+fun Point.equals(compare:Point): () -> Boolean = {
+    this.equals(compare)
+    true
+}
+
+//complex doesn't implements operator "+"
+operator fun Complex.plus(c:Complex) = this.add(c)
+
+fun main() {
+    val point = Point(10,20)
+    val point2 = Point(10,20)
+    println(-point)
+    point newfun 3
+    println(point.equals(point2))
+    //lateinit is decorate var.Include top property.
+    //lazy is a lambda ,decorate val field.
+}
---
+++ com/kotlincookbook/hogen/chapter2.kt
@@ -0,0 +1,37 @@
+package com.kotlincookbook.hogen
+
+import org.hamcrest.CoreMatchers.equalTo
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.jupiter.api.Test
+import kotlin.math.pow
+import org.junit.jupiter.api.assertAll
+import java.awt.Color
+import java.awt.Color.orange
+
+infix fun Int.`**`(x: Int) = toDouble().pow(x).toInt()
+
+
+fun main() {
+    println(100 `**` 3)
+    `doubling and halving`()
+    val color = orange
+    instFromColor(color).forEach {
+        print("${it}|")
+    }
+}
+
+@Test
+fun `doubling and halving`(){
+    assertAll("left shitfs doubling from 1", //0000_0001
+    {assertThat( 2, equalTo(1 shl 1))}
+    )
+}
+
+fun instFromColor(color: Color): List<Int>{
+    val rgb = color.rgb
+    val alpha = rgb shr 24 and 0xff
+    val red = rgb shr 16 and 0xff
+    val green = rgb shr 8 and 0xff
+    val blue = rgb and 0xff
+    return listOf(alpha,red,green,blue)
+}
